5769156fa168a939e818d8f309c15a038a7c5111
gaidenshinji <gaidenshinji2468@gmail.com>
2022-12-22 10:56:06 -0400

    /RandomCard.jsx         * Aniadida getRandomIndex(length) funcion que permite obtener 	un indice aleatorio de 0 hasta length.
    	* Seguimiento al indice de cada objeto en users, a traves de
    	un estado.
    
    	* Actualizacion del color de fondo, mediante el estado del
    	indice.
    
    	* Aniadido handleSetIndex manejador que varia el estado del
    	indice.
    
    	* Anidado componente Card que recibe un user y un color.
    
    	* Anidado boton que cambia al user actual.
    
    	* Toda la estetica de la interfaz funcional.
    
        /component/Card.jsx
            * Toda la estetica de la interfaz funcional.
    
        /assets/styles/index.css
            * Eliminado los diseños por defecto, excepto los que tienen
    	que ver con fuentes.

--- src/RandomCard.jsx
+++ src/RandomCard.jsx
@@ -1,12 +1,65 @@
 import './assets/styles/RandomCard.css';
 import users from "./data/users.json";
 import Card from "./components/Card";
+import {Shuffle} from "react-bootstrap-icons";
+import {
+    useState,
+    useEffect
+} from "react";
+
+function getRandomIndex(length)
+{
+    return Math.floor(Math.random() * length);
+}
 
 function RandomCard() 
 {
+    const [index, setIndex] = useState(0);
+    const colors = [
+	"#FDB137",
+	"#785964",
+	"#6D6875",
+	"#B5838D",
+	"#E5989B",
+	"#7E9680",
+	"#C73866",
+	"#FFB4A2",
+	"#79616F",
+	"#EAB595"
+    ];
+    // obtengo el color aleatorio para el fondo, las letras y el icono
+    // del boton
+    let color = colors[getRandomIndex(colors.length)];
+    
+    useEffect(() => {
+        // 1° Actualizo el color de fondo
+	document.body.style = `background-color: ${color};`;
+    }, [index]);
+
+    const handleSetIndex = () => {
+	// el indice aleatorio no debe superar los limites extremos
+        if(index >= 0 && index < users.length)
+	{
+            setIndex(getRandomIndex(users.length));
+	}
+    }
+
     return (
-        <div>
-	    <Card data={{}}/>
+        <div className="random-card">
+	    {/* 2° Actualiza el color del titulo */}
+	    <Card user={users[index]} color={color}/>
+	    <div id="random-button-container">
+	        <button 
+	            id="random-button"
+	            onClick={handleSetIndex}
+	        >
+	            {/* 3° Actualizo el color del icono del boton */}
+	            <Shuffle
+                        size={24}
+	                color={color}
+	            />
+	        </button>
+	    </div>
 	</div>
     );
 }  
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,44 @@ import {
     useState,
     useEffect
 } from "react";
+import {
+    EnvelopeFill,
+    TelephoneFill,
+    GeoAltFill
+} from "react-bootstrap-icons";
 
-function Card({data})
+function Card({user, color})
 {
     
     return (
-        <div>
-	</div>
+        <section id="card">
+	    <h2 style={{"color": color}}>
+	        {`${user.name.title} ${user.name.first} ${user.name.last}`}
+	    </h2>
+	    <figure>
+                <img type="image/jgp" src={user.picture.large} alt={`This is a ${user.name.first} ${user.name.last} photo`}/>
+	    </figure>
+	    <ul style={{"color": color}}>
+                <li>
+	            <EnvelopeFill
+	                size={14}
+	                color={color}
+	            />{user.email}
+	        </li>
+                <li>
+	            <TelephoneFill
+	                size={14}
+	                color={color}
+	            />{user.phone}
+	        </li>
+                <li>
+	            <GeoAltFill
+	                size={14}
+	                color={color}
+	            />{`${user.location.country} ${user.location.state} ${user.location.city}`}
+	        </li>
+	    </ul>
+	</section>
     );
 }
 
